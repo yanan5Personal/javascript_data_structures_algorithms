10d24e05fdf5f4feab002e151dd7d845c9a2a938
Ramaswamy Narayanan OB <obrnarayanan@gmail.com>
2021-11-21 17:41:21 +0530

    added modInverse

---
+++ RSA/modInverse.js
@@ -0,0 +1,29 @@
+function modInverse(e, phi) {
+  let m0 = phi,
+    t,
+    q;
+  let x0 = 0,
+    x1 = 1;
+  debugger;
+  if (phi === 1) {
+    return 0;
+  }
+
+  while (e > 1) {
+    console.log("e / phi", e / phi);
+    q = Math.floor(e / phi);
+    t = phi;
+    phi = e % phi;
+    e = t;
+    t = x0;
+    x0 = x1 - q * x0;
+    x1 = t;
+  }
+
+  if (x1 < 0) {
+    x1 += m0;
+  }
+
+  return x1;
+}
+modInverse(7, 40);
